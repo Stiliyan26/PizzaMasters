c4c9062fff04bff94acdfa58d547255dd9c9b1bb
Stiliyan26 <stili262002@abv.bg>
2022-11-14 18:07:47 +0200

    implemented register design

--- PizzaMasters/src/app/auth/auth.module.ts
+++ PizzaMasters/src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
---
+++ PizzaMasters/src/app/auth/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css', './register-responsive.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
