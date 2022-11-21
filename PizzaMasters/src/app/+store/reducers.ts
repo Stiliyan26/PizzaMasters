f603b8619d2bb65a1b37efe7fc6e0653ea47532f
Stiliyan26 <stili262002@abv.bg>
2022-11-21 20:35:00 +0200

    header logged in user bug fixed

--- PizzaMasters/src/app/+store/reducers.ts
+++ PizzaMasters/src/app/+store/reducers.ts
@@ -2,15 +2,7 @@ import { createReducer, on } from "@ngrx/store";
 import { IUser } from "../core/interfaces/user";
 import { login, logout } from "./actions";
 
-const defaultValue = {
-    username: '',
-    email: '',
-    password: '',
-    _id: '',
-    __v: 0
-}
-
-export const currentUserReducer = createReducer<IUser>(defaultValue,
+export const currentUserReducer = createReducer<IUser>(undefined,
     on(login, (_, action) => action.user),
     on(logout, () => undefined)
 )
